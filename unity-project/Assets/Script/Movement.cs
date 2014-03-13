5b7048f1d25c97bcb5920e5f422d88c0b2fbd922
DreadBoy <vertical3life@gmail.com>
2014-03-13 08:46:16 +0100

    Added hit detection and level reset.

--- unity-project/Assets/Script/Movement.cs
+++ unity-project/Assets/Script/Movement.cs
@@ -3,38 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Movement : MonoBehaviour {
-	public float speed = 1;
+	public float speed = 1f;
 	public Object člen;
 	private List<Object> členi = new List<Object>();
 	private float time_span = 0;
+    private struct orientation
+    {
+        public static Quaternion up = Quaternion.Euler(0, 0, 0);
+        public static Quaternion right = Quaternion.Euler(0, 90, 0);
+        public static Quaternion down = Quaternion.Euler(0, 180, 0);
+        public static Quaternion left = Quaternion.Euler(0, 270, 0);
+    }
 
 	// Use this for initialization
 	void Start () {
-		for(int i = 5; i > 0; i--)
-		členi.Add(Instantiate(člen, transform.position + Vector3.back * i, Quaternion.identity));
+        resetLevel();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey (KeyCode.RightArrow) && transform.localRotation != Quaternion.Euler(0, 270, 0)) {
-			transform.localRotation = Quaternion.Euler(0, 90, 0);
+		if (Input.GetKey (KeyCode.RightArrow) && transform.localRotation != orientation.left) {
+			transform.localRotation = orientation.right;
 		}
-		if (Input.GetKey (KeyCode.DownArrow) && transform.localRotation != Quaternion.Euler(0, 0, 0)) {
-			transform.localRotation = Quaternion.Euler(0, 180, 0);
+		if (Input.GetKey (KeyCode.DownArrow) && transform.localRotation != orientation.up) {
+			transform.localRotation = orientation.down;
 		}
-		if (Input.GetKey (KeyCode.LeftArrow) && transform.localRotation != Quaternion.Euler(0, 90, 0)) {
-			transform.localRotation = Quaternion.Euler(0, 270, 0);
+		if (Input.GetKey (KeyCode.LeftArrow) && transform.localRotation != orientation.right) {
+			transform.localRotation = orientation.left;
 		}
-		if (Input.GetKey (KeyCode.UpArrow) && transform.localRotation != Quaternion.Euler(0, 180, 0)) {
-			transform.localRotation = Quaternion.Euler(0, 0, 0);
+		if (Input.GetKey (KeyCode.UpArrow) && transform.localRotation != orientation.down) {
+			transform.localRotation = orientation.up;
 		}
 		time_span += Time.deltaTime;
-		if(time_span > 1){
-			time_span -= 1;
+		if(time_span > speed*0.2f){
+			time_span -= speed*0.2f;
 			moveSock();
 		}
-		//transform.position += transform.forward * Time.deltaTime * speed;
 
 
 	}
@@ -44,6 +49,34 @@ public class Movement : MonoBehaviour {
 		transform.position += transform.forward;
 		Destroy (členi[0]);
 		členi.RemoveAt (0);
+		GameObject predzadnji_člen = (GameObject) členi[členi.Count - 1];
+		predzadnji_člen.collider.enabled = true;
 		členi.Add (Instantiate(člen, transform.position - transform.forward, Quaternion.identity));
+		GameObject zadnji_člen = (GameObject)členi [členi.Count - 1];
+		zadnji_člen.collider.enabled = false;
+	}
+
+    public void takeDamage()
+    {
+        //tocke--;
+        //check of Game Over
+        resetLevel();
+    }
+
+    private void resetLevel()
+    {
+        GameObject glava = GameObject.Find("Head");
+        glava.transform.position = new Vector3(0.5f, 0.5f, 0.5f);
+        glava.transform.rotation = orientation.up;
+        for (int i = 0; i < členi.Count; i++)
+            Destroy(členi[i]);
+        členi.Clear();
+        for (int i = 5; i > 0; i--)
+            členi.Add(Instantiate(člen, transform.position - transform.forward * i, Quaternion.identity));
+    }
+
+	void OnTriggerEnter(Collider other) {
+        if (other.tag == "Enemy")
+            takeDamage();
 	}
 }
