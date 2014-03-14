6980ab6ef16e5b5fd9ee52579b6eaceb76d19d56
DreadBoy <vertical3life@gmail.com>
2014-03-14 21:57:33 +0100

    Added level2 and box trigger to load new level.
    TODO: It should load next level based on current and not always level2.

---
+++ unity-project/Assets/NextLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Head")
+            loadNextLevel();
+    }
+
+    private void loadNextLevel()
+    {
+        Application.LoadLevel("level2");
+    }
+}
--- unity-project/Assets/Script/Movement.cs
+++ unity-project/Assets/Script/Movement.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Movement : MonoBehaviour {
     public int length = 7;
 	public float speed = 1f;
-	public Object člen;
+	public Object tail_part;
     public int tocke = 5;
-	private List<Object> členi = new List<Object>();
+	private List<Object> tail = new List<Object>();
 	private float time_span = 0;
     private struct orientation
     {
@@ -28,20 +29,28 @@ public class Movement : MonoBehaviour {
 		if (Input.GetKey (KeyCode.RightArrow) && transform.localRotation != orientation.left) {
 			transform.localRotation = orientation.right;
 		}
-		if (Input.GetKey (KeyCode.DownArrow) && transform.localRotation != orientation.up) {
+		else if (Input.GetKey (KeyCode.DownArrow) && transform.localRotation != orientation.up) {
 			transform.localRotation = orientation.down;
 		}
-		if (Input.GetKey (KeyCode.LeftArrow) && transform.localRotation != orientation.right) {
+        else if (Input.GetKey(KeyCode.LeftArrow) && transform.localRotation != orientation.right)
+        {
 			transform.localRotation = orientation.left;
 		}
-		if (Input.GetKey (KeyCode.UpArrow) && transform.localRotation != orientation.down) {
+        else if (Input.GetKey(KeyCode.UpArrow) && transform.localRotation != orientation.down)
+        {
 			transform.localRotation = orientation.up;
 		}
 		time_span += Time.deltaTime;
-		if(time_span > speed*0.2f){
-			time_span -= speed*0.2f;
+        if (time_span > 0.2f / speed)
+        {
+            time_span -= 0.2f / speed;
 			moveSock();
 		}
+        if (tocke < 1)
+        {
+            GameObject.Find("Zgornja vrata").renderer.enabled = false;
+            GameObject.Find("NextLevel").collider.enabled = true;
+        }
 
 
 	}
@@ -49,13 +58,10 @@ public class Movement : MonoBehaviour {
 	private void moveSock()
 	{
 		transform.position += transform.forward;
-		Destroy (členi[0]);
-		členi.RemoveAt (0);
-		GameObject predzadnji_člen = (GameObject) členi[členi.Count - 1];
-		predzadnji_člen.collider.enabled = true;
-		členi.Add (Instantiate(člen, transform.position - transform.forward, Quaternion.identity));
-		GameObject zadnji_člen = (GameObject)členi [členi.Count - 1];
-		zadnji_člen.collider.enabled = false;
+		Destroy (tail[0]);
+		tail.RemoveAt (0);
+        tail.Add(Instantiate(tail_part, transform.position - transform.forward, Quaternion.identity));
+        ((GameObject)tail[tail.Count - 1]).collider.enabled = true;
 	}
 
     public void takeDamage()
@@ -70,16 +76,15 @@ public class Movement : MonoBehaviour {
         GameObject glava = GameObject.Find("Head");
         glava.transform.position = new Vector3(0.5f, 0.5f, 0.5f);
         glava.transform.rotation = orientation.up;
-        for (int i = 0; i < členi.Count; i++)
-            Destroy(členi[i]);
-        členi.Clear();
+        for (int i = 0; i < tail.Count; i++)
+            Destroy(tail[i]);
+        tail.Clear();
         for (int i = length; i > 0; i--)
-            členi.Add(Instantiate(člen, transform.position - transform.forward * i, Quaternion.identity));
+            tail.Add(Instantiate(tail_part, transform.position - transform.forward * i, Quaternion.identity));
     }
 
 	void OnTriggerEnter(Collider other) {
-        print("Zadeteke");
-        //if (other.tag == "Enemy")
+        if (other.tag == "Enemy")
             takeDamage();
 	}
 }
