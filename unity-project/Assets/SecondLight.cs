5180f6be0ff6eeb7e003a0c715d57ef32917eed6
unknown <zan.slana@gmail.com>
2014-05-13 10:30:02 +0200

    Added second light

---
+++ unity-project/Assets/SecondLight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject lightGameObject = new GameObject("The Light");
+        lightGameObject.AddComponent<Light>();
+        lightGameObject.light.color = Color.white;
+        lightGameObject.transform.position = new Vector3(15.3f,23.7f,-20.3f);
+        lightGameObject.transform.rotation = Quaternion.Euler(new Vector3(50f, 330f, 0f));
+        lightGameObject.light.type = LightType.Directional;
+        lightGameObject.light.intensity = 0.2f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
